97f4d67b15b3e675195fd1af060273022302d920
wo <wo@umsu.de>
2019-06-14 11:07:19 +0100

    adjust sentree.getcountermodel to new model format

--- test_sentree.js
+++ test_sentree.js
@@ -10,6 +10,16 @@ tests = {
         var m = sentree.getCounterModel();
         assertEqual(m.domain.length, 1);
         assertEqual(m.values['a'], 0);
+    },
+
+    getcountermodelfunc: function() {
+        var f = new Parser().parseFormula('Ff(ab)').negate();
+        var prover = new Prover([f]);
+        prover.pauseLength = 0;
+        prover.start();
+        var sentree = new SenTree(prover.tree, [f]);
+        var m = sentree.getCounterModel();
+        assertEqual(m.domain.length, 3);
     }
 
 }
