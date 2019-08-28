bb3907ce05442285d71dc8bb6dd01408197218ef
wo <wo@umsu.de>
2019-08-28 13:44:19 +0100

    minor improvements to page rendering and history navigation

--- index.js
+++ index.js
@@ -76,119 +76,101 @@ document.forms[0].flaField.insertAtCaret = function(str) {
    }
 }
 
-prover = null;
-onload = function(e) {
-   
-    // in case the browser has automatically filled in some value into the
-    // field (e.g. on Reload):
-    updateInput();
-
-    document.forms[0].flaField.onkeyup = updateInput;
-
-    // insert the symbol buttons on top of the text field:
-    var symButtons = ['¬','∧','∨','→','↔','∀','∃','□','◇'];
-    for (var i=0; i<symButtons.length; i++) {
-        var div = document.createElement("div");
-        div.className = "symbutton button formula";
-        div.innerHTML = symButtons[i];
-        div.onclick = symButtonClick;
-        document.getElementById("symbolButtons").appendChild(div);
-    }
-
-    function symButtonClick(e) {
+document.querySelectorAll('.symbutton').forEach(function(el) {
+    el.onclick = function(e) {
         var field = document.forms[0].flaField;
         var command = this.innerHTML;
         field.insertAtCaret(command);
         toggleAccessibilityRow();
     }
+});
 
-    // make example formulas clickable:
-    var ul = document.getElementById('exampleList');
-    for (var i=0; i<ul.childNodes.length; i++) {
-        ul.childNodes[i].onclick = function(e) {
-            document.forms[0].flaField.value = this.innerHTML;
-            toggleAccessibilityRow();
-            document.forms[0].onsubmit();
-            return false;
-        }
+var prover = null;
+function startProof(input) {
+    var parser = new Parser();
+    try {
+        var parsedInput = parser.parseInput(input);
+        var premises = parsedInput[0];
+        var conclusion = parsedInput[1];
+        var initFormulas = premises.concat([conclusion.negate()]);
     }
-
-    document.forms[0].onsubmit = function(e) {
-        // The action begins...
-        var parser = new Parser();
-        try {
-            var parsedInput = parser.parseInput(this.flaField.value);
-            var premises = parsedInput[0];
-            var conclusion = parsedInput[1];
-            var initFormulas = premises.concat([conclusion.negate()]);
-        }
-        catch (e) {
-            alert(e);
-            return false;
-        }
-        setHash(this.flaField.value);
-        document.getElementById("intro").style.display = "none";
-        document.getElementById("model").style.display = "none";
-        document.getElementById("rootAnchor").style.display = "none";
-        document.getElementById("backtostartpage").style.display = "block";
-        document.getElementById("status").style.display = "block";
-        document.getElementById("status").innerHTML = "<div id='working'>working</div>";
-
-        // Now a free-variable tableau is created. When the proof is finished,
-        // prover.finished() is called.
-        var accessibilityConstraints = [];
-        if (parser.isModal) {
-            document.querySelectorAll('.accCheckbox').forEach(function(el) {
-                if (el.checked) {
-                    // accFla = parser.parseAccessibilityFormula(el.value);
-                    accessibilityConstraints.push(el.id);
-                }
-            });
-        }
-        prover = new Prover(initFormulas, parser, accessibilityConstraints);
-        prover.onfinished = function(treeClosed) {
-            // The prover has finished. Show result:
-            var conclusionSpan = "<span class='formula'>"+conclusion+"</span>";
-            if (initFormulas.length == 1) {
-                var summary = conclusion + " is " + (treeClosed ? "valid." : "invalid.");
-            }
-            else {
-                var summary = premises.map(function(f){
-                    return "<span class='formula'>"+f+"</span>";
-                }).join(', ') + (treeClosed ? " entails " : " does not entail ") + conclusion + ".";
-            }
-            document.getElementById("status").innerHTML = summary;
-            // Translate the free-variable tableau into a sentence tableau:
-            var sentree = new SenTree(this.tree, parser); 
-            if (!treeClosed) {
-                // Tree is open. Display a countermodel if one is known:
-                if (!this.counterModel) this.counterModel = sentree.getCounterModel();
-                if (this.counterModel) {
-                    document.getElementById("model").style.display = "block";
-                    document.getElementById("model").innerHTML = "<b>Countermodel:</b><br>" +
-                        this.counterModel.toHTML();
-                    return; // shouldn't display tree because if the model was found by the modelfinder, the tree is unfinished
-                }
-            }
-            if (parser.isModal) {
-                sentree.modalize();
-            }
-            // Start painting the tree:
-            document.getElementById("rootAnchor").style.display = "block";
-            self.painter = new TreePainter(sentree, document.getElementById("rootAnchor"));
-            self.painter.paintTree();
-        }
-        setTimeout(function(){
-            prover.start();
-        }, 1);
+    catch (e) {
+        alert(e);
         return false;
     }
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("model").style.display = "none";
+    document.getElementById("rootAnchor").style.display = "none";
+    document.getElementById("backtostartpage").style.display = "block";
+    document.getElementById("status").style.display = "block";
+    document.getElementById("status").innerHTML = "<div id='working'>working</div>";
+    
+    // Now a free-variable tableau is created. When the proof is finished,
+    // prover.finished() is called.
+    var accessibilityConstraints = [];
+    if (parser.isModal) {
+        document.querySelectorAll('.accCheckbox').forEach(function(el) {
+            if (el.checked) {
+                // accFla = parser.parseAccessibilityFormula(el.value);
+                accessibilityConstraints.push(el.id);
+            }
+        });
+    }
+    prover = new Prover(initFormulas, parser, accessibilityConstraints);
+    prover.onfinished = function(treeClosed) {
+        // The prover has finished. Show result:
+        var conclusionSpan = "<span class='formula'>"+conclusion+"</span>";
+        if (initFormulas.length == 1) {
+            var summary = conclusion + " is " + (treeClosed ? "valid." : "invalid.");
+        }
+        else {
+            var summary = premises.map(function(f){
+                return "<span class='formula'>"+f+"</span>";
+            }).join(', ') + (treeClosed ? " entails " : " does not entail ") + conclusion + ".";
+        }
+        document.getElementById("status").innerHTML = summary;
+        // Translate the free-variable tableau into a sentence tableau:
+        var sentree = new SenTree(this.tree, parser); 
+        if (!treeClosed) {
+            // Tree is open. Display a countermodel if one is known:
+            if (!this.counterModel) this.counterModel = sentree.getCounterModel();
+            if (this.counterModel) {
+                document.getElementById("model").style.display = "block";
+                document.getElementById("model").innerHTML = "<b>Countermodel:</b><br>" +
+                    this.counterModel.toHTML();
+                return; // shouldn't display tree because if the model was found by the modelfinder, the tree is unfinished
+            }
+        }
+        if (parser.isModal) {
+            sentree.modalize();
+        }
+        // Start painting the tree:
+        document.getElementById("rootAnchor").style.display = "block";
+        self.painter = new TreePainter(sentree, document.getElementById("rootAnchor"));
+        self.painter.paintTree();
+    }
+    setTimeout(function(){
+        prover.start();
+    }, 1);
+    return false;
+}
    
+onload = function(e) {
+    document.forms[0].flaField.onkeyup = updateInput;
+    // in case the browser has automatically filled in some value into the
+    // field (e.g. on Reload):
+    updateInput();
+    document.forms[0].onsubmit = function(e) {
+        setHash(this.flaField.value);
+        startProof(this.flaField.value);
+        return false;
+    }
     // start proof submitted via URL (e.g. from back button):
     if (location.hash.length > 0) {
         hashChange();
     }
 }
+
 
 var hashSetByScript = false;
 function setHash(str) {
@@ -216,7 +198,8 @@ function hashChange() {
     else {
         var input = decodeURIComponent(location.hash.substr(1).replace(/\+/g, '%20'));
         document.forms[0].flaField.value = input;
-        document.forms[0].onsubmit();
+        toggleAccessibilityRow();
+        startProof(input);
     }
 }
 
